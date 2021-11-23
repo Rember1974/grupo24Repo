1b472ca7e4677c19e324e0056528e0d518b650fb
Remberto Osorio <rembertoo@uninorte.edu.co>
2021-11-22 22:38:09 -0500

    codigo casi terminado

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("HOLA MUNDO");
+let numero = 8
+
+function esprimo(num) {
+    let cont = 0
+    for (let i = 1; i <= num; i++) {
+        if (num % i == 0) {
+            cont = cont + 1
+            
+        }
+            
+    }
+}
